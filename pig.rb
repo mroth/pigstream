d87327d6a470d66f3a7796214ec6a3048432ada0
Matthew Rothenberg <mrothenberg@gmail.com>
2012-05-06 12:05:27 -0400

    progress

--- pig.rb
+++ pig.rb
@@ -11,9 +11,11 @@ OAUTH_TOKEN = ENV['OAUTH_TOKEN']
 OAUTH_TOKEN_SECRET = ENV['OAUTH_TOKEN_SECRET']
 
 #config
-TERMS = "beat level angry birds cant, beat level angry birds can't, beat level angry birds cannot"
+TERMS = ["beat level angry birds cant", "beat level angry birds can't", "beat level angry birds cannot", "dog"]
 
+#
 # configure tweetstream instance
+#
 TweetStream.configure do |config|
   config.consumer_key = CONSUMER_KEY
   config.consumer_secret = CONSUMER_SECRET
@@ -23,7 +25,9 @@ TweetStream.configure do |config|
   config.parser   = :yajl
 end
 
+#
 # configure twitter gem instance
+#
 Twitter.configure do |config|
   config.consumer_key = CONSUMER_KEY
   config.consumer_secret = CONSUMER_SECRET
@@ -31,14 +35,36 @@ Twitter.configure do |config|
   config.oauth_token_secret = OAUTH_TOKEN_SECRET
 end
 
-@client = TweetStream::Client.new
+#
+# generate the snort msg
+#
+def snortle
+  phrases=["oink", "snort", "heh heh"]
+  snort = ""
+  (3 + rand(5)).times do
+    snort += phrases[rand(phrases.size)].upcase
+    snort += " "
+  end
+  snort += "HEH HEH HEH" #always end with laugh
+  return snort
+end
 
+#
+# instantiate client and main logic
+#
+@client = TweetStream::Client.new
 @client.on_error do |message|
   puts "ERROR: #{message}"
 end
 
+$stdout.sync = true
+LIVE = false
 @client.track(TERMS) do |status|
-    puts " ** #{status.screen_name}".green + status.text.white
+    puts " ** #{status.user.screen_name} ".green + status.text.white + " (\##{status.id.to_s})"
+    if LIVE
+        puts "Posting to twitter!"
+        #Twitter.update()
+    end
 end
 
 
